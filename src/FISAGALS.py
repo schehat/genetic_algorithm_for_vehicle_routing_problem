fixed fitness evalutation and add fitness scaling

src/FISAGALS.py
@@ -1,6 +1,5 @@
-from typing import Callable
-
 import numpy as np
+from numpy import ndarray
 
 from crossover import Crossover
 from customer import Customer
@@ -10,7 +9,6 @@ from mutation import Mutation
 from vrp_instance import VRPInstance
 
 """
-    TODO updaten
     - Fitness-scaling adaptive genetic algorithm with local search
     - Chromosome representation specific integer string consisting of two parts:
         1. Number of vehicles for each depot
@@ -20,8 +18,11 @@ from vrp_instance import VRPInstance
         => first depot (index 0) has 2 vehicles, first vehicle (index 2) serves customer 1 and 2 (index 5 and 6)
         => second depot (Index 1) has 1 vehicles (index 2), serving customer 6 and 7 (index 10, 11)
 """
+
+
 class FISAGALS:
-    def __init__(self, vrp_instance: VRPInstance, population_size: int, crossover_rate: float, mutation_rate: float, max_generations: int, fitness_scaling):
+    def __init__(self, vrp_instance: VRPInstance, population_size: int, crossover_rate: float, mutation_rate: float,
+                 max_generations: int, fitness_scaling):
         self.vrp_instance: VRPInstance = vrp_instance
         self.population_size = population_size
         self.crossover_rate = Crossover(self.vrp_instance, crossover_rate)
@@ -30,7 +31,7 @@ class FISAGALS:
         self.fitness_scaling: FitnessScaling = fitness_scaling
 
     # Random initial population
-    def generate_initial_population(self):
+    def generate_initial_population(self) -> ndarray:
         initial_population = []
         for _ in range(self.population_size):
             # Part 1: Number of vehicles for each depot
@@ -58,10 +59,10 @@ class FISAGALS:
                     # centered around the avg_customers_per_vehicle
                     num_customers = int(np.random.normal(loc=avg_customers_per_vehicle, scale=std_deviation))
                     # Ensure it's within valid bounds
-                    num_customers = max(1, min(max_customers, num_customers)) 
+                    num_customers = max(1, min(max_customers, num_customers))
                     vehicle_customer_count[i] = num_customers
                 else:
-                     # If all vehicles assigned and customers remain, assign the rest to random vehicle
+                    # If all vehicles assigned and customers remain, assign the rest to random vehicle
                     num_customers = max_customers
                     i = np.random.randint(self.vrp_instance.n_vehicles)
                     vehicle_customer_count[i] += num_customers
@@ -75,10 +76,10 @@ class FISAGALS:
             chromosome = np.concatenate((depot_vehicle_count, vehicle_customer_count, order_of_customers))
             initial_population.append(chromosome)
 
-        return np.array(initial_population, dtype=object)
-    
-    def evaluate_fitness(self, chromosome):
-        # Initialize variables to keep track of fitness (total distance)
+        return np.array(initial_population, dtype=int)
+
+    def evaluate_fitness(self, chromosome: ndarray) -> float:
+        # fitness: total distance
         fitness = 0.0
         depot_index = 0
         vehicle_index = self.vrp_instance.n_depots
@@ -91,64 +92,56 @@ class FISAGALS:
             # Capacity for every vehicle the same at the moment. TODO dynamic capacity which vehicle class
             vehicle_i_capacity = 0
 
-            print("====================")
-            print(f"i: {i}")        
-            # print(f"depot_value_counter: {depot_value_counter}")
-            # print(f"vehicle_index + i: {vehicle_index + i}")
-            # print(f"vehicle_i_n_customers: {vehicle_i_n_customers}")
-
             # Check if all iterations for vehicles of current depot are done. Then continue with next depot
-            if (depot_value_counter > chromosome[depot_index]):
+            if depot_value_counter > chromosome[depot_index]:
                 depot_value_counter = 0
                 depot_index += 1
 
             vehicle_i_depot: Depot = self.vrp_instance.depots[depot_index]
 
-            # print(f"vehicle_i_depot: {vehicle_i_depot.id}")
- 
-            # -1 in range because of special treatment for last customer
-            for j in range(vehicle_i_n_customers - 1):
-                print(f"j: {j}")
-                print(f"customer_index: {customer_index + j}")
+            for j in range(vehicle_i_n_customers):
                 customer_value1 = chromosome[customer_index + j]
-                customer_value2 = chromosome[customer_index + j + 1]
-                # Using customer_value - 1 as index to pick the right customer, due to ids start by 1 not 0
-                customer_1: Customer = self.vrp_instance.customers[customer_value1 - 1]  
-                customer_2: Customer = self.vrp_instance.customers[customer_value2 - 1]  
+                # Indexing of customers starts with 1 not 0, so -1 necessary
+                customer_1: Customer = self.vrp_instance.customers[customer_value1 - 1]
 
-                # First departure of vehicle distance from depot to customer needed 
-                if (j == 0):
-                    # Add distance from depot to customer with the euclidean distance
-                    # Assuming for now that customer demand is never more than vehicle capacity
-                    fitness += np.linalg.norm(np.array([vehicle_i_depot.x, vehicle_i_depot.y]) - np.array([customer_1.x, customer_1.y]))
-                    # TODO add capacity constraint meaning vehicles with different capacity can have customer demand which is greater then their full capacity
-                    # Assuming for now that single customer demand is never more than vehicle capacity so check if capacity needed not necessary for now
+                # First iteration in loop: first trip 
+                if j == 0:
+                    # Add distance from depot to customer with the euclidean distance. Assuming single customer demand <= vehicle max capacity 
+                    fitness += np.linalg.norm(
+                        np.array([vehicle_i_depot.x, vehicle_i_depot.y]) - np.array([customer_1.x, customer_1.y]))
+
+                    # TODO add capacity constraint meaning vehicles with different capacity
+                    # Thus customer demand > vehicle max capacity possible but at least 1 vehicle exists with greater capacity
                     vehicle_i_capacity += customer_1.demand
 
-                # Check if additional customer demand exceeds vehicle capacity limit
-                # TODO Add heterogenous capacity for vehicles
-                if (vehicle_i_capacity + customer_2.demand > self.vrp_instance.max_capacity):
-                    # if capacity limit reached then trip back to depot necessary. Assuming for now heading back to same depot it came from
-                    # TODO visit different depot if possible e.g. AF-VRP charging points for robots
-                    fitness += np.linalg.norm(np.array([customer_1.x, customer_1.y]) - np.array([vehicle_i_depot.x, vehicle_i_depot.y]))
-                    
-                    # from depot to next customer
-                    fitness += np.linalg.norm(np.array([vehicle_i_depot.x, vehicle_i_depot.y]) - np.array([customer_2.x, customer_2.y]))
-                    vehicle_i_capacity = 0
-                else:
-                    # Add distance between customers
-                    fitness += np.linalg.norm(np.array([customer_1.x, customer_1.y]) - np.array([customer_2.x, customer_2.y]))
+                # Check if next customer exists in route exists
+                if j < vehicle_i_n_customers - 1:
+                    customer_value2 = chromosome[customer_index + j + 1]
+                    customer_2: Customer = self.vrp_instance.customers[customer_value2 - 1]
 
-                vehicle_i_capacity += customer_2.demand
+                    # Check customer_2 demand exceeds vehicle capacity limit
+                    # TODO Add heterogeneous capacity for vehicles
+                    if vehicle_i_capacity + customer_2.demand > self.vrp_instance.max_capacity:
+                        # Trip back to depot necessary. Assuming heading back to same depot it came from
+                        # TODO visit different depot if possible e.g. AF-VRP charging points for robots
+                        fitness += np.linalg.norm(
+                            np.array([customer_1.x, customer_1.y]) - np.array([vehicle_i_depot.x, vehicle_i_depot.y]))
+
+                        # from depot to next customer
+                        fitness += np.linalg.norm(
+                            np.array([vehicle_i_depot.x, vehicle_i_depot.y]) - np.array([customer_2.x, customer_2.y]))
+                        vehicle_i_capacity = 0
+                    else:
+                        # Add distance between customers
+                        fitness += np.linalg.norm(
+                            np.array([customer_1.x, customer_1.y]) - np.array([customer_2.x, customer_2.y]))
+
+                    vehicle_i_capacity += customer_2.demand
 
                 # Last iteration in loop, add trip from last customer to depot
-                if (j == vehicle_i_n_customers - 1):
-                    fitness += np.linalg.norm(np.array([customer_2.x, customer_2.y]) - np.array([vehicle_i_depot.x, vehicle_i_depot.y]))
-                                
-                print(f"customer 1: {customer_1.id}")
-                print(f"fitness: {fitness}")
-                print(f"Demands customer: {customer_1.demand} und {customer_2.demand} \n")
-                print(f"vehicle_i_capacity: {vehicle_i_capacity} \n")
+                if j >= vehicle_i_n_customers - 1:
+                    fitness += np.linalg.norm(
+                        np.array([customer_1.x, customer_1.y]) - np.array([vehicle_i_depot.x, vehicle_i_depot.y]))
 
             customer_index += vehicle_i_n_customers
             depot_value_counter += 1
@@ -157,11 +150,10 @@ class FISAGALS:
 
     def run(self):
         population = self.generate_initial_population()
-        print(population[0])
 
-        #for generation in range(self.max_generations):
-            # Evaluate fitness of the population
-        fitness_scores = self.evaluate_fitness(population[0])
+        for generation in range(self.max_generations):
+            fitness_scores = np.array([(int(i), self.evaluate_fitness(chromosome)) for i, chromosome in enumerate(population)], dtype=np.dtype([('index', int), ('fitness', float)]))
+            self.fitness_scaling(fitness_scores)
         print(fitness_scores)
 
         #     # Selection

src/crossover.py
@@ -6,14 +6,14 @@ from vrp_instance import VRPInstance
 
 
 class Crossover:
-    adaptive_crossover_rate = 0.5 #TODO
+    adaptive_crossover_rate = 0.5  # TODO
 
-    def __init__(self, vrp_instance: VRPInstance, CROSSOVER_RATE: float):
+    def __init__(self, vrp_instance: VRPInstance, crossover_rate: float):
         self.vrp_instance: VRPInstance = vrp_instance
         self.START_SECOND_PART = vrp_instance.n_depots
         self.START_THIRD_PART = self.START_SECOND_PART + vrp_instance.n_vehicles
         self.END_THIRD_PART = self.START_THIRD_PART + vrp_instance.n_customers
-        self.CROSSOVER_RATE = CROSSOVER_RATE
+        self.CROSSOVER_RATE = crossover_rate
 
     # first and second part of chromosome
     def uniform(self, parent1: np.ndarray, parent2: np.ndarray) -> np.ndarray:
@@ -34,7 +34,7 @@ class Crossover:
                 child[i] = parent2[i]
 
         return child
-    
+
     # third part of chromosome
     def order(self, parent1: np.ndarray, parent2: np.ndarray) -> np.ndarray:
         # Check if crossover should occur

src/fitness_scaling.py
@@ -1,6 +1,23 @@
-class FitnessScaling():
-    def linear():
+import numpy as np
+from numpy import ndarray
+
+
+class FitnessScaling:
+    def linear(self):
         pass
-    
-    def power_rank():
-        pass
+
+    @staticmethod
+    def power_rank(fitness: ndarray):
+        fitness.sort(order='fitness')
+
+        # Calculate the rank (actual index) for each fitness value
+        rank = np.arange(len(fitness))
+
+        # Calculate the power of the rank with 1.005 and replace raw fitness
+        fitness['fitness'] = (rank + 1) ** 1.005
+
+        # Calculate sum of all powers in parallel
+        sum_of_powers = np.sum(fitness['fitness'])
+
+        # Normalize the calculated power fitness
+        fitness['fitness'] /= sum_of_powers

src/main.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python3
 
-from timeit import default_timer as timer
 
 import numpy as np
 
-from crossover import Crossover
 from customer import Customer
 from depot import Depot
 from FISAGALS import FISAGALS
 from fitness_scaling import FitnessScaling
-from mutation import Mutation
 from vrp_instance import VRPInstance
 
 
-def read_cordeau_instance(file_path):
+def read_cordeau_instance(file_path) -> VRPInstance:
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
     # Parse the first line to get instance information
     header = lines[0].split()
     n_vehicles, n_customers, n_depots = map(int, header[1:4])
-    max_capacity = lines[1].split()[1]
-    
+    max_capacity = int(lines[1].split()[1])
+
     customers = np.zeros((n_customers,), dtype=Customer)
     depots = np.zeros((n_depots,), dtype=Depot)
 
-     # Read customer data
-    for i, line in enumerate(lines[n_vehicles + 1 : n_vehicles + 1 + n_customers]):
+    # Read customer data
+    for i, line in enumerate(lines[n_vehicles + 1: n_vehicles + 1 + n_customers]):
         data = line.split()
         if len(data) >= 5:
             customer = Customer(int(data[0]), float(data[1]), float(data[2]), int(data[4]))
             customers[i] = customer
 
     # Read depot data
-    for i, line in enumerate(lines[n_vehicles + 1 + n_customers :]):
+    for i, line in enumerate(lines[n_vehicles + 1 + n_customers:]):
         data = line.split()
         if len(data) >= 3:
-            # depot id is + n_customers offset, unfavorable in later stages of GA  
+            # depot id is + n_customers offset, unfavorable in later stages of GA
             depot = Depot(int(data[0]) - n_customers, float(data[1]), float(data[2]))
             depots[i] = depot
 
-    vrp_instance = VRPInstance(n_vehicles, n_customers, n_depots, max_capacity, customers, depots)
-  
-    return vrp_instance
+    return VRPInstance(n_vehicles, n_customers, n_depots, max_capacity, customers, depots)
+
 
 if __name__ == "__main__":
     np.set_printoptions(threshold=np.inf)
+    # Set the print options to control the display format
+
     # TEST MUTATION AND CROSSOVER
     # mutation_rate = 1.0
     # crossover_rate = 1.0
@@ -60,14 +58,10 @@ if __name__ == "__main__":
     #     # mutation.uniform(chromosome1)
     #     crossover_new1 = crossover.order(chromosome1, chromosome2)
     #     end = timer()
-        
+
     #     # print(end - start)
     #     # print(f"{chromosome1}")
     #     print(crossover_new1)
-
-    # Define parameters
-    instance_file_path = "../benchmark/C-mdvrp/p01"        
-    # vrp_instance = read_cordeau_instance(instance_file_path)
 
     # TESTING WITHOUT BENCHMARK
     n_customers = 12
@@ -75,13 +69,13 @@ if __name__ == "__main__":
     customers = np.zeros((n_customers,), dtype=Customer)
     depots = np.zeros((n_depots,), dtype=Depot)
     for i in range(12):
-        customer = Customer(i+1, i*10, i*5, i*10)
+        customer = Customer(i+1, i*10, i*5, (1+i)*5)
         customers[i] = customer
     for i in range(2):
         depot = Depot(i+1, 10**(i+1), 50*i)
         depots[i] = depot
     vrp_instance = VRPInstance(4, n_customers, n_depots, 80, customers, depots)
-    
+
     # print("Customer Data:")
     # for customer in vrp_instance.customers:
     #     print(f"Customer {customer.id}")
@@ -96,10 +90,13 @@ if __name__ == "__main__":
     #     print(f"  X-coordinate: {depot.x}")
     #     print(f"  Y-coordinate: {depot.y}")
 
-    population_size = 100
+    # instance_file_path = "../benchmark/C-mdvrp/p01"
+    # vrp_instance = read_cordeau_instance(instance_file_path)
+
+    population_size = 50
     crossover_rate = 0.8
     mutation_rate = 0.1
-    max_generations = 1000
+    max_generations = 100
     fitness_scaling = FitnessScaling.power_rank
 
     # Run it GA

src/mutation.py
@@ -5,8 +5,8 @@ import numpy as np
 from vrp_instance import VRPInstance
 
 
-class Mutation():
-    adaptive_mutation_rate = 1 #TODO
+class Mutation:
+    adaptive_mutation_rate = 1  # TODO
 
     def __init__(self, vrp_instance: VRPInstance, mutation_rate: float):
         self.vrp_instance: VRPInstance = vrp_instance
@@ -20,7 +20,7 @@ class Mutation():
         # Define the range for mutated numbers of vehicles
         min_vehicle = 0
         max_vehicle = self.vrp_instance.n_vehicles
-        
+
         # Iterate through the depots and apply uniform mutation
         for i in range(self.START_SECOND_PART):
             if random.random() <= self.mutation_rate:
@@ -31,7 +31,7 @@ class Mutation():
         # Vehicle can have 0 customers to server TODO valid? 
         min_customer = 0
         # max customers limited that every other vehicle can have at least one customer
-        max_customer = self.vrp_instance.n_customers #TODO - self.vrp_instance.n_vehicles + 1
+        max_customer = self.vrp_instance.n_customers  # TODO - self.vrp_instance.n_vehicles + 1
 
         # Iterate through the vehicles and apply uniform mutation
         for i in range(self.START_SECOND_PART, self.START_THIRD_PART):
@@ -45,8 +45,8 @@ class Mutation():
     def repair_procedure(self, chromosome: np.ndarray):
         # iterate through depots
         sum_vehicles = np.sum(chromosome[:self.START_SECOND_PART])
-        diff = sum_vehicles - self.vrp_instance.n_vehicles 
-        
+        diff = sum_vehicles - self.vrp_instance.n_vehicles
+
         while diff != 0:
             random_index = np.random.randint(0, self.START_SECOND_PART)
 
@@ -62,7 +62,7 @@ class Mutation():
 
         # iterate through vehicles
         sum_customers = np.sum(chromosome[self.START_SECOND_PART:self.START_THIRD_PART])
-        diff = sum_customers - self.vrp_instance.n_customers 
+        diff = sum_customers - self.vrp_instance.n_customers
 
         while diff != 0:
             random_index = np.random.randint(self.START_SECOND_PART, self.START_THIRD_PART)
@@ -78,7 +78,7 @@ class Mutation():
     # third part of chromosome in place
     def swap(self, chromosome: np.ndarray):
         # Check if mutation should occur
-        if random.random() <= self.mutation_rate:            
+        if random.random() <= self.mutation_rate:
             positions = self._generate_distinct_positions(2)
             pos1, pos2 = positions[0], positions[1]
 
@@ -87,7 +87,7 @@ class Mutation():
 
     # third part of chromosome in place
     def inversion(self, chromosome: np.ndarray):
-       # Check if mutation should occur
+        # Check if mutation should occur
         if random.random() <= self.mutation_rate:
             pos1, pos2 = self._generate_distinct_positions(2)
             # Ensure pos1 < pos2