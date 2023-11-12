fixed distance calculation and plotting color bug

src/main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from GA1 import GA1
+from GA import GA
 from fitness_scaling import power_rank
 from selection import n_tournaments
 from local_search import two_opt_complete, two_opt_single
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     np.set_printoptions(threshold=np.inf)
 
     # Create vrp instance
-    INSTANCE_FILE_PATH = "../benchmark/C-mdvrp/p04"
+    INSTANCE_FILE_PATH = "../benchmark/C-mdvrp/p01"
     VRP_INSTANCE = read_cordeau_instance(INSTANCE_FILE_PATH)
 
     # Set GA parameters
@@ -95,20 +95,20 @@ if __name__ == "__main__":
     SELECTION_METHOD = n_tournaments
     LOCAL_SEARCH_COMPLETE = two_opt_complete
     LOCAL_SEARCH_SINGLE = two_opt_single
-    tournament_size = 4
+    tournament_size = 3
     elitism_percentage = 0.05
 
     # Configure GA and run
-    ga = GA1(VRP_INSTANCE,
-             POPULATION_SIZE,
-             CROSSOVER_RATE,
-             MUTATION_RATE,
-             MAX_GENERATIONS,
-             INITIAL_POPULATION,
-             FITNESS_SCALING,
-             SELECTION_METHOD,
-             LOCAL_SEARCH_COMPLETE,
-             LOCAL_SEARCH_SINGLE,
-             tournament_size=tournament_size,
-             elitism_percentage=elitism_percentage)
+    ga = GA(VRP_INSTANCE,
+            POPULATION_SIZE,
+            CROSSOVER_RATE,
+            MUTATION_RATE,
+            MAX_GENERATIONS,
+            INITIAL_POPULATION,
+            FITNESS_SCALING,
+            SELECTION_METHOD,
+            LOCAL_SEARCH_COMPLETE,
+            LOCAL_SEARCH_SINGLE,
+            tournament_size=tournament_size,
+            elitism_percentage=elitism_percentage)
     ga.run()